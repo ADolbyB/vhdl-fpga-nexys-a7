384aa66e93bac8d03b031580fac8b19dc03afd7a
Joel Brigida <joel@drtechreps.com>
2023-02-13 13:17:22 -0500

    Edit BCD and add HEX to 7segment

--- Assignments/HW2/HEX_to_7segment.vhd
+++ Assignments/HW2/HEX_to_7segment.vhd
@@ -1,23 +1,23 @@
 -- Joel Brigida
 -- CDA 4240C: Digital Design Lab
--- This is a dataflow model for a Binary Coded Decimal to 7 segment display.
--- BCD Input is used to display numbers 0 - 9 (A - F are excluded)
+-- This is a dataflow model for a Hexadecimal to 7 segment display.
+-- HEX input is used to display digits 0 - 9 and A - F
 -- This is for a Common Cathode Display: Logic '1' required to illuminate segments
 
 library IEEE;
 use ieee.std_logic_1164.all;
 
-Entity BCD_to_7segment is
+Entity HEX_to_7segment is
     Port(
-        BCD : in std_logic_vector(3 downto 0); -- 4 bit BCD signal
+        HEX : in std_logic_vector(3 downto 0); -- 4 bit HEX signal
         DISP : out std_logic_vector(6 downto 0) -- 7 Segment Display
     );
-End Entity BCD_to_7Segment;
+End Entity HEX_to_7segment;
 
-Architecture dataflow of BCD_to_7segment is
+Architecture dataflow of HEX_to_7segment is
 
 begin
-    with BCD select
+    with HEX select
           -- abcdefg display segments
     DISP <= "1111110" when "0000", -- Displays '0'
             "0110000" when "0001", -- Displays '1'
@@ -29,6 +29,12 @@ begin
             "1110000" when "0111", -- Displays '7'
             "1111111" when "1000", -- Displays '8'
             "1100011" when "1001", -- Displays '9'
-            "0000000" when OTHERS;
+            "1110111" when "1010", -- Displays 'A'
+            "0011111" when "1011", -- Displays 'b'
+            "1001110" when "1100", -- Displays 'C'
+            "0111101" when "1101", -- Displays 'd'
+            "1001111" when "1110", -- Displays 'E'
+            "1000111" when "1111", -- Displays 'F'
+            "0000000" when OTHERS; -- Turn off Display
 
 end dataflow;
