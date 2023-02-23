ac880e4e78395371ce4f06b49cf26626a3c7b270
Joel Brigida <joel@drtechreps.com>
2023-02-22 22:25:54 -0500

    Add Nbit Register & Edit 4bit Register

--- examples/register/reg_Nbit_parallel.vhd
+++ examples/register/reg_Nbit_parallel.vhd
@@ -1,24 +1,25 @@
 -- Joel Brigida
 -- CDA 4240C: Digital Design Lab
--- This is an example of an 4-bit Parallel Load Register
--- This Register uses 4 Flip Flops.
+-- This is an example of an N-bit Parallel Load Register
+-- This Register uses N Flip Flops.
 
 library IEEE;
 USE IEEE.std_logic_1164.all;
 
-Entity reg_4bit_parallel is
+Entity reg_Nbit_parallel is
+    generic (N : integer := 8);
     PORT (
-        D : IN std_logic_vector(3 downto 0);
+        D : IN std_logic_vector(N-1 downto 0);
         Enable : IN std_logic;
         Load : IN std_logic;    -- Load = 1: [Q3, Q2, Q1, Q0]
         ShiftIn : IN std_logic; -- Load = 0: [ShiftIn, Q3, Q2, Q1]
         Clock : IN std_logic;
-        Q : OUT std_logic_vector(3 downto 0)
+        Q : OUT std_logic_vector(N-1 downto 0)
     );
-end entity reg_4bit_parallel;
+end entity reg_Nbit_parallel;
 
-architecture behavioral of reg_4bit_parallel is
-    signal QT : std_logic_vector(3 downto 0);
+architecture behavioral of reg_Nbit_parallel is
+    signal QT : std_logic_vector(N-1 downto 0);
 begin
     process (Clock)
     begin
@@ -27,7 +28,7 @@ begin
                 if Load = '1' then -- Do Not Shift In
                     QT <= D;
                 else               -- Shift In a bit, Drop Q0.
-                    QT <= ShiftIn & QT(3 downto 1);
+                    QT <= ShiftIn & QT(N-1 downto 1);
                 end if;
             end if;
         end if;
