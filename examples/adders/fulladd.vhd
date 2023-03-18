8651bf55a0c712eab9eb1c55257c30a7938f2289
Joel Brigida <joel@drtechreps.com>
2023-03-18 14:32:30 -0400

    Fix Syntax For Full Adder Logic

--- examples/adders/fulladd.vhd
+++ examples/adders/fulladd.vhd
@@ -1,21 +1,24 @@
+-- Joel Brigida
+-- CDA 4240C: Digital Design Lab
 -- VHDL program for a Full Adder
+
 library ieee; -- Library declaration
 use ieee.std_logic_1164.all; -- Use all definitions from steady state logic package
 
 ENTITY fulladd is
     PORT(
-        x : IN STD_LOGIC;
-        y : IN STD_LOGIC;
-        cin : in STD_LOGIC;
-        sum : OUT STD_LOGIC;
-        cout: OUT STD_LOGIC
+        X : IN STD_LOGIC;
+        Y : IN STD_LOGIC;
+        CIN : in STD_LOGIC;
+        SUM : OUT STD_LOGIC;
+        COUT : OUT STD_LOGIC
     );
 END ENTITY fulladd;
 
-architecture fulladd_dataflow OF fulladd is
+architecture dataflow OF fulladd is
 begin
     -- Dataflow implementation describes gate level implementation
-    sum <= x XOR y XOR cin;
-    cout <= (x AND y) OR (cin AND x) OR (cin AND y);
+    SUM <= X XOR Y XOR CIN;
+    cout <= (X AND Y) OR (CIN AND (X XOR Y));
 
-end fulladd_dataflow;
+end dataflow;
