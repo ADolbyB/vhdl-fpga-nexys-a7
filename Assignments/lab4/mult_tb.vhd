a24aefbad9de56dafb33c1ee7a7ca7823c5aad0a
Joel Brigida <joel@drtechreps.com>
2023-03-31 21:14:24 -0400

    Complete CSM Test Bench

--- Assignments/lab4/mult_tb.vhd
+++ Assignments/lab4/mult_tb.vhd
@@ -26,14 +26,14 @@ architecture behavioral of mult_tb is
         GENERIC(N : integer := 8);
         PORT(
             CLK : IN STD_LOGIC;
-            A : IN std_logic_vector(N-1 downto 0);      -- 8 bit inputs
-            B : IN std_logic_vector(N-1 downto 0);
-            PROD : OUT std_logic_vector(2*N-1 downto 0) -- 16 bit output
+            A_mult : IN std_logic_vector(N-1 downto 0);     -- 8 bit inputs
+            B_mult : IN std_logic_vector(N-1 downto 0);
+            PROD : OUT std_logic_vector(2*N-1 downto 0)     -- 16 bit output
         );
     end component;
     
     CONSTANT N : integer := 8;
-    CONSTANT CLK_PER : time := 10ns; -- create period constant(CLOCK?)
+    CONSTANT CLK_PER : time := 10ns;
     
     signal CLK_tb : std_logic;
     signal A_tb : std_logic_vector(N-1 downto 0);
@@ -42,47 +42,54 @@ architecture behavioral of mult_tb is
     
 begin
     -- Instantiation Of Components
+    CLK_gen: process -- generate the process for the clock similar to previous lab 0 and lab 2
+        begin
+            CLK_tb <= '0';
+            wait for CLK_PER / 2;
+            CLK_tb <= '1';
+            wait for CLK_PER / 2;
+        end process;
+    
     multiplier : mult -- instantiate mult component
+        GENERIC MAP(N => N)
         PORT MAP (
-            A => A_tb,
-            B => B_tb,
+            A_mult => A_tb,
+            B_mult => B_tb,
             CLK => CLK_tb,
             PROD => PROD_tb
         );
 
-    clk_gen: process -- generate the process for the clock similar to previous lab 0 and lab 2
-        begin
-            clk_tb <= '0';
-            wait for CLK_PER;
-            clk_tb <= '1';
-            wait for CLK_PER;
-        end process;
-    
-    test_bench : process -- Begin Test Bench Process:
-        variable cur_line   : integer := 1; -- variables for reading for MULT_FILE
-        variable v_line     : line;
-        variable v_space    : character;
-        variable v_a        : std_logic_vector(7 downto 0);
-        variable v_b        : std_logic_vector(7 downto 0);
-        variable v_p_exp    : std_logic_vector(15 downto 0);
+    test_bench : process                        -- Begin Test Bench Process:
+        variable current_line : integer := 1;   -- variables for reading MULT_FILE
+        variable v_line       : line;
+        variable v_space      : character;
+        variable v_a          : std_logic_vector(7 downto 0);
+        variable v_b          : std_logic_vector(7 downto 0);
+        variable v_prod_exp   : std_logic_vector(15 downto 0);
 
     begin
 
-        -- useful functions 
-        -- while not endfile("FILE"): keeps reading until reaching the end of the file.
-        -- readline("FILE","VARIABLE"): reads 1 line at a time from file and stores it in variable.
-        -- hread("VARIABLE", "VARIABLE"): reads from the 1st variable into the 2nd variable in HEX format.
-        -- read("VARIABLE", "VARIABLE"): reads from the 1st variable into the 2nd variable.
+        WHILE NOT endfile(MULT_FILE) LOOP   -- keeps reading until reaching the end of the file.
+            READLINE(MULT_FILE, v_line);    -- reads 1 line at a time from file and stores it in variable.
+            HREAD(v_line, v_a);             -- reads from the 1st variable into the 2nd variable in HEX format.
+            READ(v_line, v_space);          -- reads from the 1st variable into the 2nd variable.
+            HREAD(v_line, v_b);
+            READ(v_line, v_space);
+            HREAD(v_line, v_prod_exp);
+            A_tb <= v_a;                    -- after reading the data into each variable, set your signals equal to the variables
+            B_tb <= v_b;
+            wait for 2 * CLK_PER;
 
-        -- you must replace "FILE" and "VARIABLE" with your file name and appropriate variable names.
-        
-        -- after reading the data into each variable, set your signals equal to the variables
-        -- and wait for 2*PERIOD. Next, assert that your product is equal to the expected product,
-        -- and then start reading from the next line. If all the products are correct, report that the
-        -- simulation was complete.
+            ASSERT PROD_tb = v_prod_exp
+                REPORT "Actual Result Not Matching Expected Result At Line #" & integer'image(current_line)
+                    severity failure;
 
+            current_line := current_line + 1;
+        end loop;
+
+        REPORT "SIMULATION SUCCESS!!";      -- If all the products are correct, report that the simulation was successful.
         wait;
-    
+
     end process;
 
 end behavioral;
