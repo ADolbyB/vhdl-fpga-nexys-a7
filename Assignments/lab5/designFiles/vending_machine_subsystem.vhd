0be9f886e932d52a02df131183657a1e4adc7df6
Joel Brigida <joel@drtechreps.com>
2023-04-16 02:34:08 -0400

    Refactor & Debug Subsystem Wrapper

--- Assignments/lab5/designFiles/vending_machine_subsystem.vhd
+++ Assignments/lab5/designFiles/vending_machine_subsystem.vhd
@@ -1,13 +1,14 @@
 -- Joel Brigida
 -- CDA 4240C: Digital Design Lab
--- This is the wrapper for the Vending Machine Subsystem.
+-- This is the wrapper for the Vending Machine Subsytem.
 
+-- WRAPPER FOR SUBSYSTEM
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY vending_machine_subsystem is
     Port ( 
-            -- inputs
+        -- inputs
         clk             : in std_logic;
         rst             : in std_logic;
         soda_sel        : in std_logic_vector(3 downto 0);
@@ -15,7 +16,7 @@ ENTITY vending_machine_subsystem is
         lock            : in std_logic;
         coin_push       : in std_logic;
         coin_sel        : in std_logic_vector(1 downto 0);
-            --outputs
+        --outputs
         soda_reserved   : out std_logic;
         soda_price      : out std_logic_vector(11 downto 0);
         soda_drop       : out std_logic;
@@ -45,14 +46,14 @@ architecture Behavioral of vending_machine_subsystem is
     
     component deposit_register is
         Port ( 
-            -- inputs
+                -- inputs
             clk      : in std_logic;
             rst      : in std_logic;
             incr     : in std_logic;
             incr_amt : in std_logic_vector(11 downto 0);
             decr     : in std_logic;
             decr_amt : in std_logic_vector(11 downto 0);
-            --outputs
+                --outputs
             amt      : out std_logic_vector(11 downto 0)
         );
     end component;
@@ -78,85 +79,84 @@ architecture Behavioral of vending_machine_subsystem is
             error_reserved : out std_logic
             );
     end component;
-
+    
     -- Soda List
-    signal soda_price_list       : std_logic_vector(11 downto 0);
-    signal soda_select_list      : std_logic_vector(3 downto 0);
-    signal soda_reserved_list    : std_logic;
+    signal soda_list_price        : std_logic_vector(11 downto 0);
+    signal soda_list_reserved     : std_logic;
     
     -- Coin List
-    signal coin_select_signal    : std_logic_vector(1 downto 0);
-    signal coin_amount_signal    : std_logic_vector(11 downto 0);
-
+    signal coin_list_amount       : std_logic_vector(11 downto 0);
+    
     -- Deposit Register
-    signal depreg_clk_signal     : std_logic;
-    signal depreg_rst_signal     : std_logic;
-    signal incr_signal           : std_logic;
-    signal incr_amt_signal       : std_logic_vector(11 downto 0);
-    signal decr_signal           : std_logic;
-    signal decr_amt_signal       : std_logic_vector(11 downto 0);
-    signal amt_signal            : std_logic_vector(11 downto 0);
-
+    signal deposit_incr_flag      : std_logic;
+    signal deposit_incr_amt       : std_logic_vector(11 downto 0);
+    signal deposit_decr_flag      : std_logic;
+    signal deposit_decr_amt       : std_logic_vector(11 downto 0);
+    signal deposit_amt_signal     : std_logic_vector(11 downto 0) := X"000";
+    
     -- Vending Machine Controller
-    signal vend_clk_signal       : std_logic;
-    signal vend_rst_signal       : std_logic;
-    signal lock_signal           : std_logic;
-    signal soda_reserved_ctrl    : std_logic;
-    signal soda_price_ctrl       : std_logic_vector(11 downto 0);
-    signal soda_req_signal       : std_logic;
-    signal deposit_amt_signal    : std_logic_vector(11 downto 0);
-    signal coin_push_signal      : std_logic;
-    signal coin_amt_signal       : std_logic_vector(11 downto 0);
-    signal soda_drop_signal      : std_logic;
-    signal deposit_incr_signal   : std_logic;
-    signal deposit_decr_signal   : std_logic;
-    signal coin_reject_signal    : std_logic;
-    signal error_amt_signal      : std_logic;
-    signal error_reserved_signal : std_logic;
+    signal vend_soda_reserved     : std_logic;
+    signal vend_soda_price        : std_logic_vector(11 downto 0);
+    signal vend_deposit_amt       : std_logic_vector(11 downto 0);
+    signal vend_coin_amt          : std_logic_vector(11 downto 0);
+    signal vend_deposit_incr_flag : std_logic;
+    signal vend_deposit_decr_flag : std_logic;
 
 begin
-
+    
     SODA_LIST_INST : soda_list
     PORT MAP(
-        soda_price => soda_price_list,
-        soda_sel => soda_select_list,
-        soda_reserved => soda_reserved_list
+        soda_price => soda_list_price,           -- Signal
+        soda_sel => soda_sel,                    -- Top Level Subsystem
+        soda_reserved => soda_list_reserved      -- Signal
     );
 
     COIN_LIST_INST : coin_list
     PORT MAP(
-        coin_sel => coin_select_signal,
-        coin_amt => coin_amount_signal
+        coin_sel => coin_sel,                    -- Top Level Subsystem
+        coin_amt => coin_list_amount             -- Signal
     );
 
     DEPOSIT_REG_INST : deposit_register
     PORT MAP(
-        clk => depreg_clk_signal,
-        rst => depreg_rst_signal,
-        incr => incr_signal,
-        incr_amt => incr_amt_signal,
-        decr => decr_signal,
-        decr_amt => decr_amt_signal,
-        amt => amt_signal
+        clk => clk,                              -- Top Level Subsystem
+        rst => rst,                              -- Top Level Subsystem
+        incr => deposit_incr_flag,               -- Signal
+        incr_amt => deposit_incr_amt,            -- Signal
+        decr => deposit_decr_flag,               -- Signal
+        decr_amt => deposit_decr_amt,            -- Signal
+        amt => deposit_amt_signal                -- Signal
     );
 
     VEND_CONTROL_INST : vending_machine_ctrl
     PORT MAP(
-        clk => vend_clk_signal,
-        rst => vend_rst_signal,
-        lock => lock_signal,
-        soda_reserved => soda_reserved_ctrl,
-        soda_price => soda_price_ctrl,
-        soda_req => soda_req_signal,
-        deposit_amt => deposit_amt_signal,
-        coin_push => coin_push_signal,
-        coin_amt => coin_amt_signal,
-        soda_drop => soda_drop_signal,
-        deposit_incr => deposit_incr_signal,
-        deposit_decr => deposit_decr_signal,
-        coin_reject => coin_reject_signal,
-        error_amt => error_amt_signal,
-        error_reserved => error_reserved_signal
+        clk => clk,                              -- Top Level Subsystem
+        rst => rst,                              -- Top Level Subsystem
+        lock => lock,                            -- Top Level Subsystem
+        soda_reserved => vend_soda_reserved,     -- Signal
+        soda_price => vend_soda_price,           -- Signal
+        soda_req => soda_req,                    -- Top Level Subsystem
+        deposit_amt => vend_deposit_amt,         -- Signal
+        coin_push => coin_push,                  -- Top Level Subsystem
+        coin_amt => vend_coin_amt,               -- Signal
+        soda_drop => soda_drop,                  -- Top Level Subsystem
+        deposit_incr => vend_deposit_incr_flag,  -- Signal
+        deposit_decr => vend_deposit_decr_flag,  -- Signal
+        coin_reject => coin_reject,              -- Top Level Subsystem
+        error_amt => error_amt,                  -- Top Level Subsystem
+        error_reserved => error_reserved         -- Top Level Subsystem
     );
+    
+    vend_coin_amt <= coin_list_amount;            -- Send Coin List 'coin_amt' output to Vending Machine Controller 'coin_amt' input.
+    deposit_incr_amt <= coin_list_amount;         -- Send Coin List 'coin_amt' output to Deposit Register 'incr_amt' input.
+    deposit_decr_amt <= soda_list_price;          -- Send Soda List 'soda_price' output to Deposit Register 'decr_amt' input.
+    vend_soda_price <= soda_list_price;           -- Send Soda List 'soda_price' output to Vending Machine Controller 'soda_price' input.
+    soda_price <= soda_list_price;                -- Send Soda List 'soda_price' output to the Outer Vending Machine System.
+    vend_soda_reserved <= soda_list_reserved;     -- Send Soda List 'soda_reserved' output to Vending Machine Controller 'soda_reserved' input.
+    soda_reserved <= soda_list_reserved;          -- Send Soda List 'soda_reserved' output to the Outer Vending Machine System.
+    deposit_incr_flag <= vend_deposit_incr_flag;  -- Send Vending Machine Controller 'deposit_incr' to Deposit Register 'incr' input.
+    deposit_decr_flag <= vend_deposit_decr_flag;  -- Send Vending Machine Controller 'deposit_decr' to Deposit Register 'decr' input.
+    vend_deposit_amt <= deposit_amt_signal;       -- Send Deposit Register 'amt' output to Vending Machine Controller 'deposit_amt' input.
+    deposit_amt <= deposit_amt_signal;            -- Send Deposit Register 'amt' output to Outer Vending Machine System.
 
 end Behavioral;
