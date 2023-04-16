8fb5dbaded3a74ced98fa8d94beba3f503e9e2d7
Joel Brigida <joel@drtechreps.com>
2023-04-16 02:34:47 -0400

    Debug & Refactor Deposit Register

--- Assignments/lab5/designFiles/deposit_register.vhd
+++ Assignments/lab5/designFiles/deposit_register.vhd
@@ -3,53 +3,45 @@
 -- This is the Deposit Register for the Vending Machine
 -- This Component accounts the amount deposited in cents.
 
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/03/2023 12:36:31 AM
--- Design Name: 
--- Module Name: deposit_register - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
+-- DEPOSIT REGISTER: accounts the amount deposited OR Decremented in cents.
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
--- Need Unsigned Only??
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 
 entity deposit_register is
     Port ( 
         -- inputs
-        clk             : in std_logic;
-        rst             : in std_logic;
-        incr            : in std_logic;
-        incr_amt        : in std_logic_vector(11 downto 0);
-        decr            : in std_logic;
-        decr_amt        : in std_logic_vector(11 downto 0);
+        clk      : in std_logic;
+        rst      : in std_logic;    
+        incr     : in std_logic;
+        incr_amt : in std_logic_vector(11 downto 0);
+        decr     : in std_logic;
+        decr_amt : in std_logic_vector(11 downto 0);
         --outputs
-        amt             : out std_logic_vector(11 downto 0)
+        amt      : out std_logic_vector(11 downto 0)
     );
 end deposit_register;
 
 architecture Behavioral of deposit_register is
+
+    SIGNAL TOTAL_AMT : std_logic_vector(11 downto 0) := X"000"; -- Set inital Total Amt to $0.00
     
-
-
 begin
+    
+    DEP_REG : PROCESS(rst, clk)
+    BEGIN
+        IF rising_edge(CLK) THEN
+            IF (rst = '0') THEN
+                amt <= X"000";                                                            -- Reset deposit Credit
+                TOTAL_AMT <= X"000";           
+            ELSIF (incr = '1') THEN
+                TOTAL_AMT <= std_logic_vector(unsigned(TOTAL_AMT) + unsigned(incr_amt));  -- Update Total Amount Intermediate Value
+                amt <= std_logic_vector(unsigned(TOTAL_AMT) + unsigned(incr_amt));        -- Update Output Amount
+            ELSIF (decr = '1') THEN
+                TOTAL_AMT <= std_logic_vector(unsigned(TOTAL_AMT) - unsigned(decr_amt));  -- Update Total Amount Intermediate Value
+                amt <= std_logic_vector(unsigned(TOTAL_AMT) - unsigned(decr_amt));        -- Update Output Amount
+            END IF;
+        END IF;       
+    END PROCESS;
 
 end Behavioral;
