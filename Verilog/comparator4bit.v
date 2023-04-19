e85fd54a7d192ad6a2e38fa70168c166ca13c400
Joel Brigida <joel@drtechreps.com>
2023-04-18 22:20:11 -0400

    2 channel 4 bit comparator

---
+++ Verilog/comparator4bit.v
@@ -0,0 +1,23 @@
+/*
+Joel Brigida
+CDA 4240C: Digital Design Lab
+This is a 2 channel 4bit comparator example
+*/
+
+module compare(A, B, AeqB, AgtB, AltB);
+    input [3:0] A, B;
+    output reg AeqB, AgtB, AltB;
+
+    always @(A, B) begin
+        AeqB = 0;
+        AgtB = 0;
+        AltB = 0;
+        if (A == B)
+            AeqB = 1;
+        else if (A > B)
+            AgtB = 1;
+        else
+            AltB = 1;
+    end
+
+endmodule
